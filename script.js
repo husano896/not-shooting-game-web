258059969edbf84ddee7df1f1fc969596321d934
xFly.Xie <Fly.Xie@grandsys.com>
2022-09-30 16:29:53 +0800

    加入判定動畫以及軌道效果 & 鍵盤綁定

--- script.js
+++ script.js
@@ -1,27 +1,50 @@
-var keypressed = {};
+const keypressed = {};
 
 // 常數定義區
-var gameLane = document.querySelector('div.game-lane');
-var cursorL = document.querySelector('div.laser-cursor-l');
-var cursorR = document.querySelector('div.laser-cursor-r');
+const gameLane = document.querySelector('div.game-lane');
+const cursorL = document.querySelector('div.laser-cursor-l');
+const cursorR = document.querySelector('div.laser-cursor-r');
 
-var laneBTA = document.querySelector('div.bt-a');
-var laneBTB = document.querySelector('div.bt-b');
-var laneBTC = document.querySelector('div.bt-c');
-var laneBTD = document.querySelector('div.bt-d');
-var laneFXL = document.querySelector('div.fx-l');
-var laneFXR = document.querySelector('div.fx-r');
+const laneBTA = document.querySelector('div.bt-a');
+const laneBTB = document.querySelector('div.bt-b');
+const laneBTC = document.querySelector('div.bt-c');
+const laneBTD = document.querySelector('div.bt-d');
+const laneFXL = document.querySelector('div.fx-l');
+const laneFXR = document.querySelector('div.fx-r');
 
-var camera = document.querySelector('.camera');
+let laserLVal = 0;
+let laserRVal = 0;
 
-var perspective = 128;
-var rotateX = 10;
-var rotateZ = 0;
+const cameraEl = document.querySelector('.camera');
 
-var notes = [];
+const audioRes = {
+	tick1: new Audio('audio/tick1.mp3'),
+	tick2: new Audio('audio/tick2.mp3')
+}
+let keyA = 'q';
+let keyB = 'w';
+let keyC = 'o';
+let keyD = 'p';
+let keyL = 'z';
+let keyR = '.';
+let keyVolLL = '1';
+let keyVolLR = '2';
+let keyVolRL = '9';
+let keyVolRR = '0';
+/* perspective越小, zoom-bottom 越大 zoom-top越小; */
+let perspective = 128;
 
-var lanes = ['bt-a', 'bt-b', 'bt-c', 'bt-d', 'fx-l', 'fx-r']
-var lastUpdate = 0;
+/* rotateX越大, zoom-bottom越大 zoom-top越小 */
+let rotateX = 30;
+let rotateZ = 0;
+
+let notes = [];
+
+let lanes = ['bt-a', 'bt-b', 'bt-c', 'bt-d', 'fx-l', 'fx-r']
+let lastUpdate = 0;
+
+// 音樂所在位置ms
+let musicPos = 0;
 class Note {
 	constructor(type) {
 		this.el = document.createElement('div')
@@ -67,31 +90,123 @@ class Note {
 	}
 }
 function update(delta) {
-	
 	notes.forEach(note => note.update(delta - lastUpdate));
 	notes = notes.filter(note => note.el);
 	lastUpdate = delta;
+
+	// 旋鈕的判定圖案開關
+	cursorL.setAttribute('state', laserLVal < 200 ? '' : 'miss')
+	cursorR.setAttribute('state', laserRVal > 800 ? '' : 'miss')
 	window.requestAnimationFrame(update);
+	if (keypressed[keyVolLL]) {
+		laserLVal = Math.max(0, Math.min(laserLVal - 5, 1000));
+	} else if (keypressed[keyVolLR]) {
+		laserLVal = Math.max(0, Math.min(laserLVal + 5, 1000));
+	}
+	if (keypressed[keyVolRL]) {
+		laserRVal = Math.max(0, Math.min(laserRVal - 5, 1000));
+	} else if (keypressed[keyVolRR]) {
+		laserRVal = Math.max(0, Math.min(laserRVal + 5, 1000));
+	}
+	cursorL.style.left = `calc(${laserLVal / 10}% - 7.5vh)`
+	cursorR.style.left = `calc(${laserRVal / 10}% - 7.5vh)`
+
+	updateCamera();
 }
 
+function updateCamera() {
+	cameraEl.style.perspective = `${perspective}px`;
+
+	/* rotateX越大, zoom-bottom越大 zoom-top越小*/
+	gameLane.style.transform = `rotateX(${rotateX}deg) rotateY(0deg) rotateZ(${rotateZ}deg)`;
+}
+
+
+function judgeKey(key) {
+	const judgeEl = document.createElement('div');
+	judgeEl.className = 'lane-judge empty';
+
+	const judgeTextEl = document.createElement('div');
+	judgeTextEl.className = 'judge-text critical';
+	judgeTextEl.innerText = "CRITIAL";
+
+	switch (key) {
+		case keyA:
+			laneBTA.appendChild(judgeEl);
+			laneBTA.appendChild(judgeTextEl);
+			setTimeout(() => { laneBTA.removeChild(judgeEl); laneBTA.removeChild(judgeTextEl); }, 500);
+			audioRes.tick1.currentTime = 0;
+			audioRes.tick1.play();
+			break;
+		case keyB:
+			laneBTB.appendChild(judgeEl);
+			laneBTB.appendChild(judgeTextEl);
+			setTimeout(() => { laneBTB.removeChild(judgeEl); laneBTB.removeChild(judgeTextEl); }, 500);
+			audioRes.tick1.currentTime = 0;
+			audioRes.tick1.play();
+			break;
+		case keyC:
+			laneBTC.appendChild(judgeEl);
+			laneBTC.appendChild(judgeTextEl);
+			setTimeout(() => { laneBTC.removeChild(judgeEl); laneBTC.removeChild(judgeTextEl); }, 500);
+			audioRes.tick1.currentTime = 0;
+			audioRes.tick1.play();
+			break;
+		case keyD:
+			laneBTD.appendChild(judgeEl);
+			laneBTD.appendChild(judgeTextEl);
+			setTimeout(() => { laneBTD.removeChild(judgeEl); laneBTD.removeChild(judgeTextEl); }, 500);
+			audioRes.tick1.currentTime = 0;
+			audioRes.tick1.play();
+			break;
+		case keyL:
+			laneFXL.appendChild(judgeEl);
+			laneFXL.appendChild(judgeTextEl);
+			setTimeout(() => { laneFXL.removeChild(judgeEl); laneFXL.removeChild(judgeTextEl); }, 500);
+			audioRes.tick2.currentTime = 0;
+			audioRes.tick2.play();
+			break;
+		case keyR:
+			laneFXR.appendChild(judgeEl);
+			laneFXR.appendChild(judgeTextEl);
+			setTimeout(() => { laneFXR.removeChild(judgeEl); laneFXR.removeChild(judgeTextEl); }, 500);
+			audioRes.tick2.currentTime = 0;
+			audioRes.tick2.play();
+			break;
+	}
+}
 function init() {
 	document.addEventListener('keydown', (ev) => {
 		// console.log(ev);
 		ev.preventDefault();
-		if (ev.key === 'ArrowUp') {
-			perspective -= 1;
-		}
-		if (ev.key === 'ArrowDown') {
-			perspective += 1;
-		}
-		if (ev.key === 'ArrowLeft') {
-			rotateZ -= 1;
-		}
-		if (ev.key === 'ArrowRight') {
-			rotateZ += 1;
+		switch (ev.key) {
+			case 'ArrowUp':
+				rotateX -= 0.1;
+				break;
+			case 'ArrowDown':
+				rotateX += 0.1;
+				break;
+			case 'ArrowLeft':
+				rotateZ -= 1;
+				break;
+			case 'ArrowRight':
+				rotateZ += 1;
+				break;
+			case keyA:
+			case keyB:
+			case keyC:
+			case keyD:
+			case keyL:
+			case keyR:
+				if (keypressed[ev.key]) {
+					break;
+				}
+				judgeKey(ev.key);
+				break;
 		}
 
-		camera.style.perspective = `${perspective}px`;
+
+		cameraEl.style.perspective = `${perspective}px`;
 		gameLane.style.transform = `rotateX(${rotateX}deg) rotateY(0deg) rotateZ(${rotateZ}deg)`;
 		keypressed[ev.key] = true;
 
@@ -102,16 +217,18 @@ function init() {
 	});
 	document.addEventListener('pointermove', (ev) => {
 		ev.preventDefault();
-		cursorL.style.left = `calc(min(${ev.clientX}px, 60vh) - 7.5vh)`
-		cursorR.style.left = `calc(min(${ev.clientY}px, 60vh) - 7.5vh)`
+		laserLVal = Math.max(0, Math.min(laserLVal + ev.movementX, 1000));
+		laserRVal = Math.max(0, Math.min(laserRVal + ev.movementY, 1000));
+		// cursorL.style.left = `calc(${laserLVal / 10}% - 7.5vh)`
+		// cursorR.style.left = `calc(${laserRVal / 10}% - 7.5vh)`
 	});
-	camera.style.perspective = `${perspective}px`;
-	gameLane.style.transform = `rotateX(${rotateX}deg) rotateY(0deg) rotateZ(${rotateZ}deg)`;
 	update();
+	updateCamera();
 }
 
 init()
 
+/*
 setInterval(() => {
 	notes.push(new Note(lanes[Math.floor(Math.random()* lanes.length )] ))
-}, 100)
+}, 100)*/
