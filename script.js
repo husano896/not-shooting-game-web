83ae44a22294a4fd34f6cf56c9781ef9eced352a
xFly.Xie <Fly.Xie@grandsys.com>
2022-10-03 11:59:02 +0800

    add serviceworker

--- script.js
+++ script.js
@@ -273,3 +273,12 @@ init()
 setInterval(() => {
 	notes.push(new Note(lanes[Math.floor(Math.random()* lanes.length )] ))
 }, 100)*/
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/serviceworker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err))
+  })
+}
